38c9b77f29756647cd118f351d2c1a3627f422bd
Benjamin Röhrl <b.roehrl@gmail.com>
2016-05-25 16:33:54 +0200

    - he logger now also logs to a file

--- src/router/logger.go
+++ src/router/logger.go
@@ -3,10 +3,24 @@ package router
 import (
 	"fmt"
 	"git"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
+
+func init() {
+	logfile, err := os.OpenFile("commit-finder.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		multi := io.MultiWriter(os.Stdout)
+		log.SetOutput(multi)
+		log.Fatalf("error opening file: %v", err)
+	} else {
+		multi := io.MultiWriter(os.Stdout, logfile)
+		log.SetOutput(multi)
+	}
+}
 
 // Logger wraps a log output around a http handler
 func Logger(inner http.Handler, name string) http.Handler {
